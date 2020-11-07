20c67f1e83115aaff9933278a8cf26df8462657a
mfirdous <mfirdous@mail.bradley.edu>
2020-11-07 16:44:29 -0600

    Create TestCase2.java

---
+++ phase1Base/src/phase1Base/TestCase2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phase1Base;
+
+/**
+ *
+ * @author yun
+ */
+public class TestCase2 {
+    
+    //TODO 
+    //Repeat for all cases.
+    
+}
