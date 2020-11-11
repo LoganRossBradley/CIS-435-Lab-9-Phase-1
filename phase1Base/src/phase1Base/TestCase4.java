f940296ab93f4e65be66d4049b362d474d792c41
LoganRossBradley <60478655+LoganRossBradley@users.noreply.github.com>
2020-11-10 20:07:17 -0600

    Minor changes and added test case 4&5 placeholders

---
+++ phase1Base/src/phase1Base/TestCase4.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phase1Base;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import static phase1Base.Common.*;
+import static phase1Base.TestingMethods.*;
+
+
+/**
+ * @author #04 | Moom Firdous 
+ * @author #10 | Meridith Kcoh 
+ * @author #12 | James Mckean
+ * @author #15 | Logan Ross
+ */
+public class TestCase3 {
+    public static void main(String[] args) {
+
+        
+        String case01 = "CASE #0_1 | Initialize Sender (Fixed Data) ";
+        System.out.println(caseSeperator("*", case01));
+
+        // true for using fixed msg, ks, and hash value, other wise random data
+        boolean fixedData = true;
+        User amySender = createSender("Amy", fixedData);
+        getRSAKeys(amySender);
+        System.out.println("==> Sender's Status | " + amySender.toString() + "\n");
+
+        String case02 = "CASE #0_2 | Initialize Receiver (Fixed Data) ";
+
+        System.out.println(caseSeperator("*", case02));
+
+        User bobReceiver = createReceiver("Bob");
+        getRSAKeys(bobReceiver);
+
+        System.out.println("==> Receiver's Status | " + bobReceiver.toString() + "\n");
+
+    }
+
+}
+
+
